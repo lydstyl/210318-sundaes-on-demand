ab761b7b9e9e34464a0d081d74068c0cac7cc90c
Gabriel Brun <lydstyl@gmail.com>
2021-03-22 12:42:38 +0100

    userEvent instead of fireEvent

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { SummaryForm } from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 const termsCheckboxName = /Terms and Conditions/i;
 const buttonName = /confirm order/i;
@@ -16,7 +17,7 @@ it("Checking checkbox enables button", () => {
   const checkbox = screen.getByRole("checkbox", { name: termsCheckboxName });
   const button = screen.getByRole("button", { name: buttonName });
 
-  fireEvent.click(checkbox);
+  userEvent.click(checkbox);
 
   expect(checkbox).toBeChecked();
   expect(button).toBeEnabled();
@@ -26,9 +27,14 @@ it("Unchecking checkbox again disables button", () => {
   const checkbox = screen.getByRole("checkbox", { name: termsCheckboxName });
   const button = screen.getByRole("button", { name: buttonName });
 
-  fireEvent.click(checkbox);
-  fireEvent.click(checkbox);
+  userEvent.click(checkbox);
+  userEvent.click(checkbox);
 
   expect(checkbox).not.toBeChecked();
   expect(button).toBeDisabled();
+});
+it("Popover responds to hover", () => {
+  // popover starts out hidden
+  // popover appears upon mouseover of checkbox label
+  // popover disappears when we mous out
 });
