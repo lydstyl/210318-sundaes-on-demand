dca24f32d0076db7fb405f4c733eb70dfc65942e
Gabriel Brun <lydstyl@gmail.com>
2021-03-22 12:31:29 +0100

    checkbox and button

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { SummaryForm } from "./pages/summary/SummaryForm";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeDisabled();
+it("test", () => {
+  // render(<SummaryForm />);
+  // const checkbox = screen.getByRole("checkbox", { name: /terms-checkbox/i });
+  // expect(checkbox).not.toBeChecked();
 });
