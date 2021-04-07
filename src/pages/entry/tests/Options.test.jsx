1d709a6d06d5c35ec5479a67ab3818498118876b
Gabriel Brun <lydstyl@gmail.com>
2021-04-07 11:39:13 +0200

    await findBy element async

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -2,11 +2,11 @@ import { render, screen } from "@testing-library/react";
 
 import Options from "../Options";
 
-test("Displays image for each scoop option from server", () => {
+test("Displays image for each scoop option from server", async () => {
   render(<Options optionType="scoops" />);
 
-  // find images
-  const scoopImages = screen.getAllByRole("img", { name: /scoop$/i });
+  // find images anytime ascyn we use await and find by
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
   // confirm alt text of images
