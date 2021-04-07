5d127451cc9507ed3c49d7a426889a594dbeeae5
Gabriel Brun <lydstyl@gmail.com>
2021-04-07 12:23:07 +0200

    async topping options

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -2,6 +2,7 @@ import Row from "react-bootstrap/Row";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
@@ -10,14 +11,15 @@ export default function Options({ optionType }) {
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
-      .then((response) => setItems(response.data))
+      .then((response) => {
+        setItems(response.data);
+      })
       .catch((error) => {
         // TODO: handle error response
       });
   }, [optionType]);
 
-  // TODO: replace 'null' with ToppingOption when available
-  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
   const optionItems = items.map((i) => (
     <ItemComponent key={i.name} name={i.name} imagePath={i.imagePath} />
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -5,7 +5,7 @@ import Options from "../Options";
 test("Displays image for each scoop option from server", async () => {
   render(<Options optionType="scoops" />);
 
-  // find images anytime ascyn we use await and find by
+  // find images. Anytime ascync we use await and find by
   const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
@@ -13,4 +13,21 @@ test("Displays image for each scoop option from server", async () => {
   const altTexts = scoopImages.map((img) => img.alt);
 
   expect(altTexts).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+it("Displays image for each topping option from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+
+  expect(toppingImages).toHaveLength(3);
+
+  const altTexts = toppingImages.map((img) => img.alt);
+  expect(altTexts).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
 });
