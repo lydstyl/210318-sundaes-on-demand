18c44e0a6fe2193aad090c180f32f74a3198ffe4
Gabriel Brun <lydstyl@gmail.com>
2021-04-20 18:35:57 +0200

    toppings subtotal test

--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,10 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../../test-utils/testing-library-utils";
 import userEvent from "@testing-library/user-event";
 import Option from "../Options";
-import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
+// import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
 
-it("update scoop subtotal when scoops change", async () => {
-  render(<Option optionType="scoops" />, { wrapper: OrderDetailsProvider });
+it("Update scoops subtotal when scoops change", async () => {
+  render(<Option optionType="scoops" />);
 
   // make sur total starts out $0.00
   const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
@@ -25,4 +25,32 @@ it("update scoop subtotal when scoops change", async () => {
   userEvent.clear(chocolateInput);
   userEvent.type(chocolateInput, "2");
   expect(scoopsSubtotal).toHaveTextContent("6.00");
+});
+
+it("Update toppings subtotal when toppings change", async () => {
+  render(<Option optionType="toppings" />);
+
+  // ● Assert on default toppings subtotal
+  const toppingsSubtotal = screen.getByText("Toppings total: $", {
+    exact: false,
+  });
+  expect(toppingsSubtotal).toHaveTextContent("0.00");
+
+  // ● Find and tick one box, assert on updated subtotal
+  const cherriesCheckbox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  userEvent.click(cherriesCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
+
+  // ● Tick another box on, assert on subtotal
+  const hotFudgeCheckbox = await screen.findByRole("checkbox", {
+    name: "Hot fudge",
+  });
+  userEvent.click(hotFudgeCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent("3.00");
+
+  // ● Tick one of the boxes off (click it again) and assert on subtotal
+  userEvent.click(cherriesCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
 });
