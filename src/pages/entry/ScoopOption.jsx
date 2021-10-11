ec8f3af2cced9f5ca34da6f2d085d9e9fc11aa3d
Gabriel Brun <lydstyl@gmail.com>
2021-10-11 14:09:01 +0200

    Fix test "Disable order button for no scoops".

--- src/pages/entry/ScoopOption.jsx
+++ src/pages/entry/ScoopOption.jsx
@@ -5,44 +5,44 @@ import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 
 export default function ScoopOptions({ name, imagePath, updateItemCount }) {
-  const [isValid, setIsValid] = useState(false);
+    const [isValid, setIsValid] = useState(false);
 
-  const handleChange = (event) => {
-    const val = parseFloat(event.target.value);
+    const handleChange = (event) => {
+        const val = parseFloat(event.target.value);
 
-    if (val <= 0 || val > 10 || !(Math.floor(val) === val)) {
-      setIsValid(false);
-    } else {
-      setIsValid(true);
-      updateItemCount(name, val);
-    }
-  };
+        if (val < 0 || val > 10 || !(Math.floor(val) === val)) {
+            setIsValid(false);
+        } else {
+            setIsValid(true);
+            updateItemCount(name, val);
+        }
+    };
 
-  return (
-    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
-      <img
-        style={{ width: "75%" }}
-        src={`http://localhost:3030/${imagePath}`}
-        alt={`${name} scoop`}
-      />
+    return (
+        <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
+            <img
+                style={{ width: "75%" }}
+                src={`http://localhost:3030/${imagePath}`}
+                alt={`${name} scoop`}
+            />
 
-      <Form.Group
-        controlId={`${name}-count`}
-        as={Row}
-        style={{ marginTop: "10px" }}
-      >
-        <Form.Label column xs="6" style={{ textAlign: "right" }}>
-          {name}
-        </Form.Label>
-        <Col xs="5" style={{ textAlign: "left" }}>
-          <Form.Control
-            type="number"
-            defaultValue={0}
-            onChange={handleChange}
-            isInvalid={!isValid}
-          />
+            <Form.Group
+                controlId={`${name}-count`}
+                as={Row}
+                style={{ marginTop: "10px" }}
+            >
+                <Form.Label column xs="6" style={{ textAlign: "right" }}>
+                    {name}
+                </Form.Label>
+                <Col xs="5" style={{ textAlign: "left" }}>
+                    <Form.Control
+                        type="number"
+                        defaultValue={0}
+                        onChange={handleChange}
+                        isInvalid={!isValid}
+                    />
+                </Col>
+            </Form.Group>
         </Col>
-      </Form.Group>
-    </Col>
-  );
+    );
 }
